544fbdbbc993be6d177336bd5736a49e26b89e97
Jenny Lin <misslinyang91@gmail.com>
2021-09-07 17:36:33 +1000

    created the game loop

--- app.js
+++ app.js
@@ -20,3 +20,19 @@ if (Math.floor(Math.random() * 2) === 0) {
 if (Math.floor(Math.random() * 2) === 0) {
   yV = -yV;
 }
+
+//the game loop
+function runGame() {
+  //moving the ball from the current position
+  xP += xV;
+  yP += yV;
+  //clearing the canvas
+  canvasContext.clearRect(0, 0, canvasEl.width, canvasEl.height);
+  //drawing the ball
+  canvasContext.beginPath();
+  canvasContext.fillStyle = "orange";
+  canvasContext.arc(xP, yP, radius, 0, Math.PI * 2);
+  canvasContext.fill();
+}
+
+setInterval(runGame, 1000 / FPS);
