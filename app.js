aac4501f377fc80b717e0553ce3fb47d051f2678
Jenny Lin <misslinyang91@gmail.com>
2021-09-07 17:14:55 +1000

    created the velocities

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 const canvasEl = document.querySelector("canvas");
 const canvasContext = canvasEl.getContext("2d");
+
+const FPS = 60;
+let radius = 50;
+let xP, yP;
+let xV, yV;
+
+xP = canvasEl.width / 2;
+yP = canvasEl.height / 2;
+//pixels per frame
+xV = Math.floor(Math.random() * 201 + 99) / FPS;
+yV = Math.floor(Math.random() * 201 + 99) / FPS;
+
+//coin toss situation
+if (Math.floor(Math.random() * 2) === 0) {
+  xV = -xV;
+}
+
+if (Math.floor(Math.random() * 2) === 0) {
+  yV = -yV;
+}
